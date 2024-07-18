2ad6f46cb66cd14e85b82389bbc01ac56a7774dd
Chinmayee R <128812723+chinmayee-s-r@users.noreply.github.com>
2024-07-18 23:05:11 +0530

    Add files via upload

---
+++ Assign1_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int Display(){
+    int i=0;
+    i = 5;
+
+    while(i>0){
+        printf("%d ",i);
+        i--;
+    }
+}
+
+int main(){
+    Display();
+
+    return 0;
+}
